b0be2d2110d9d4914c8fcaf41a7ddca9793f3ccf
mbatilando <mari.batilando@gmail.com>
2014-01-03 22:15:44 -0800

    Finish type, image

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,18 +7,57 @@ angular.module('myPokedexApp')
 			'AngularJS',
 			'Karma'
 		];
-		console.log("yi");
 
-		var pokemonUrl = 'http://pokeapi.co/api/v1/pokemon/' + $routeParams.pokemonId;
-		//console.log($routeParams.pokemonId);
-		$http.get(pokemonUrl)
+		var mainPokemonUrl = 'http://pokeapi.co/api/v1/pokemon/' + $routeParams.pokemonId;
+		
+
+		$http.get(mainPokemonUrl)
 		    .success(function (data, status, headers, config) {
 		        console.log(data);
 		        $scope.pokemonData = data;
+		        getPokemonDesc(data);
+		        getPokemonSprite(data);
+		    	getPokemonType(data);
 		    })
 		    .error(function (data, status, headers, config) {
-		        console.log("Failed");
+		        console.log('Main Pokemon Data Fetch Failed');
 		});
 
+
+		var getPokemonDesc = function(data) {
+			var pokemonDescUrl = 'http://pokeapi.co' + data.descriptions[0].resource_uri;
+			$http.get(pokemonDescUrl)
+			    .success(function (data, status, headers, config) {
+			        console.log("GOT IT");
+			        $scope.pokemonDesc = data;
+			    })
+			    .error(function (data, status, headers, config) {
+			        console.log('Pokemon Desc Data Fetch Failed');
+			});
+		};
+
+		var getPokemonSprite = function(data) {
+			var pokemonSpriteUrl = 'http://pokeapi.co' + data.sprites[0].resource_uri;
+			$http.get(pokemonSpriteUrl)
+			    .success(function (data, status, headers, config) {
+			        console.log("GOT IT");
+			        $scope.pokemonSprite = data;
+			    })
+			    .error(function (data, status, headers, config) {
+			        console.log('Pokemon Desc Data Fetch Failed');
+			});
+		};
+		
+		var getPokemonType = function(data) {
+			var pokemonType = "";
+			var pokemonTypeList = data.types;
+			
+			for(var i = 0, len = pokemonTypeList.length; i < len; i++) {
+				pokemonType += pokemonTypeList[i].name + "/";
+			}
+
+
+			$scope.pokemonType = pokemonType.slice(0, pokemonType.length - 1);
+		}
 
 	});
